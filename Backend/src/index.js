d3d376ffc81300d843c6bec123496c3c1c4b950a
Wasif Hafeez <wasifhafeez26@gmail.com>
2025-12-15 19:58:53 +0500

    deployment fix@1.2

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -32,7 +32,7 @@ app.use("/api/messages", messageRoute);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../Frontend/dist")));
 
-  app.get("*", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "../Frontend", "dist", "index.html"));
   });
 }
