ec5b3aeab906f2b677be9d9da397215a8e5bafad
Wasif Hafeez <wasifhafeez26@gmail.com>
2025-12-15 17:26:03 +0500

    fix build issue@0.1

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -30,10 +30,10 @@ app.use("/api/auth", authRoute);
 app.use("/api/messages", messageRoute);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../Frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../Frontend", "dist", "index.html"));
   });
 }
 
